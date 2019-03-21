ba34269312a42aeb40ef4fb20935b0919fe664f2
Annaia Berry <jarcane@gmail.com>
2019-03-21 13:36:32 +0200

    Tough topbar layout

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,8 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Element exposing (..)
-import Element.Font as Font
 import Element.Background as Background
+import Element.Font as Font
 import Html exposing (Html)
 import Types as T
 
@@ -37,35 +37,57 @@ update msg model =
 
 ---- VIEW ----
 
+
+statGroup : Model -> Element Msg
+statGroup model =
+    row
+        [ spacing 40
+        , centerX
+        ]
+        [ el [] (text "⏲ -20 h")
+        , el [] (text "📊 0 %")
+        , el [] (text "☀ 6 days")
+        ]
+
+
+avatarDrop : Model -> Element Msg
+avatarDrop model =
+    el [ alignRight ] (text "\u{1F913}")
+
+
 topBar : Model -> Element Msg
 topBar model =
-    row 
-        [ width fill 
+    row
+        [ width fill
         , height <| px 80
-        , padding 20
+        , paddingXY 50 20
+        , spacing 20
         , Background.color <| rgb255 0 54 67
         , Font.color <| rgb 1 1 1
-        ] 
-        [ text "FutuHours" ]
+        ]
+        [ el [ alignLeft ] (text "FutuHours")
+        , statGroup model
+        , avatarDrop model
+        ]
 
 
 hoursList : Model -> Element Msg
 hoursList model =
-    row 
-    [ Background.color <| rgb255 227 236 236
-    , width fill
-    , height fill
-    , scrollbars
-    , padding 20 
-    ] 
-    [ text "I'm the body " ]
+    row
+        [ Background.color <| rgb255 227 236 236
+        , width fill
+        , height fill
+        , scrollbars
+        , padding 20
+        ]
+        [ text "I'm the body " ]
 
 
 mainLayout : Model -> Element Msg
 mainLayout model =
-    column 
-        [ width fill 
-        , height fill 
+    column
+        [ width fill
+        , height fill
         ]
         [ topBar model
         , hoursList model
@@ -74,7 +96,7 @@ mainLayout model =
 
 view : Model -> Html Msg
 view model =
-    Element.layout 
+    Element.layout
         [ Font.family [ Font.sansSerif ] ]
         (mainLayout model)
 
