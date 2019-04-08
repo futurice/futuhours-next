cd77f005441f5ad65801738720b0b2e81dd983d6
Annaia Berry <jarcane@gmail.com>
2019-04-08 16:25:38 +0300

    Add API proxy

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const express = require('express');
+const proxy = require('express-http-proxy');
 const path = require('path');
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
+const API_URL = process.env.API_URL;
+
 const app = express();
 
+// Static resources
 app.use(express.static(path.join(__dirname + "/build", '/')));
+
+// API proxy
+app.use('/api', proxy(API_URL));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
