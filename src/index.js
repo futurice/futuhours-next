f7289b7c40f3e71e3f6ec7cca58b02e9648899d6
rachhek <rachhek@gmail.com>
2021-06-17 11:47:26 +0300

    set default value for futucortexIframUrl env variable by using ternary operator

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './main.css';
 import { Elm } from './Main.elm';
 import { unregister } from './registerServiceWorker';
 
+const futucortexIframeUrl = process.env.ELM_APP_FUTUCORTEX_IFRAME_URL? process.env.ELM_APP_FUTUCORTEX_IFRAME_URL: ""
+
 Elm.Main.init({
   node: document.getElementById('root'),
   flags: { 
     now: Date.now(),
     width: window.innerWidth,
     height: window.innerHeight,
-    futucortexIframeUrl: process.env.ELM_APP_FUTUCORTEX_IFRAME_URL
+    futucortexIframeUrl: futucortexIframeUrl
   }
 });
 
